89a6e91f32d2610ed22d50190d19695638c21564
Forever <dragonwarrior920619@gmail.com>
2020-12-23 16:49:07 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# backend-api-go
+A simple RESTful API (CRUD) built with GoLang
+
+## Requirements
+
+- [Git](https://git-scm.com/)
+- [Docker](https://www.docker.com/)
+- [docker-compose](https://docs.docker.com/compose/)
+
+## Install
+Clone and setup:
+
+```bash
+$ git clone https://github.com/lovelyrrg51/backend-api-go
+$ cd backend-api-go
+$ ./setup.sh
+```
+
+## Running
+First, setup docker-compose:
+
+```bash
+$ docker-compose up
+```
+
+Then start querying at `http://localhost:3000/api/users/`
+
+## Shutdown
+```bash
+$ docker-compose down
+```
+
+## APIs
+The entity **User** has the following fields:
+
+- ID (uint)
+- name (string)
+- email (string)
+- age (int)
+- mobile (string)
+
+Follows the list of users APIs:
+
+|METHOD|URL|REQUEST HEADERS|REQUEST PAYLOAD|RESPONSE HEADERS|RESPONSE PAYLOAD|
+|------|---|---------------|---------------|----------------|----------------|
+|GET|http://localhost:3000/api/users/ | | | |User[]|
+|POST|http://localhost:3000/api/users/ |Content-Type: "application/json"|User||User|
+|GET|http://localhost:3000/api/users/10 | | | |User|
+|PUT|http://localhost:3000/api/users/10 |Content-Type: "application/json"|User||User|
+|DELETE|http://localhost:3000/api/users/10 | | | | |
---
+++ main.go
@@ -0,0 +1,149 @@
+package main
+
+import (
+	"github.com/go-martini/martini"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	log "github.com/Sirupsen/logrus"
+	"os"
+	"net/http"
+	"encoding/json"
+	"github.com/martini-contrib/render"
+	"github.com/martini-contrib/cors"
+)
+
+func getUsers(db *gorm.DB, render render.Render) {
+	var users []User
+	db.Find(&users)
+
+	render.JSON(http.StatusOK, users)
+}
+
+func insertUser(req *http.Request, db *gorm.DB, render render.Render) {
+	decoder := json.NewDecoder(req.Body)
+	defer req.Body.Close()
+
+	var user User
+	err := decoder.Decode(&user)
+	if err != nil {
+		log.Error(err)
+		render.Text(http.StatusBadRequest, "Bad JSON encoding")
+		return
+	}
+
+	db.Create(&user)
+	db.Save(&user)
+
+	render.JSON(http.StatusCreated, user)
+}
+
+func getUser(params martini.Params, db *gorm.DB, render render.Render) {
+	var user User
+	if db.First(&user, params["id"]).RecordNotFound() {
+		render.Text(http.StatusNotFound, "User not found")
+		return
+	}
+
+	render.JSON(http.StatusOK, user)
+}
+
+func updateUser(req *http.Request, params martini.Params, db *gorm.DB, render render.Render) {
+	var user User
+	if db.First(&user, params["id"]).RecordNotFound() {
+		render.Text(http.StatusNotFound, "User not found")
+		return
+	}
+
+	decoder := json.NewDecoder(req.Body)
+	defer req.Body.Close()
+
+	var userData User
+	err := decoder.Decode(&userData)
+	if err != nil {
+		log.Error(err)
+		render.Text(http.StatusBadRequest, "Bad JSON encoding")
+		return
+	}
+
+	if len(userData.Name) > 0 {
+		user.Name = userData.Name
+	}
+
+	if userData.Age > 0 {
+		user.Age = userData.Age
+	}
+
+	if len(userData.Email) > 0 {
+		user.Email = userData.Email
+	}
+
+	if len(userData.Mobile) > 0 {
+		user.Mobile = userData.Mobile
+	}
+
+	db.Save(&user)
+
+	render.JSON(http.StatusOK, user)
+}
+
+func deleteUser(params martini.Params, db *gorm.DB, render render.Render) {
+	var user User
+	if db.First(&user, params["id"]).RecordNotFound() {
+		render.Text(http.StatusNotFound, "User not found")
+		return
+	}
+
+	db.Delete(&user)
+
+	render.Text(http.StatusOK, "User deleted")
+}
+
+type User struct {
+	gorm.Model
+
+	Name string `json:"name",omitempty;gorm:"default:'name'"`
+	Age int `json:"age",omitempty;gorm:"default:'age'"`
+	Email string `json:"email",omitempty;gorm:"default:'email'"`
+	Mobile string `json:"mobile",omitempty;gorm:"default:'mobile'"`
+}
+
+func main() {
+	log.Info("Starting the web server...")
+
+	log.Info("Opening the SQLite DB...")
+
+	db, err := gorm.Open("sqlite3", "./data.db")
+	if err != nil {
+		log.Error(err)
+		os.Exit(1)
+	}
+	defer db.Close()
+
+	log.Info("SQLite DB opened!")
+
+	db.AutoMigrate(&User{})
+
+	server := martini.Classic()
+
+	server.Map(db)
+	server.Use(render.Renderer())
+	server.Use(cors.Allow(&cors.Options{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "OPTIONS", "PUT", "DELETE", "POST"},
+		AllowHeaders:     []string{"Origin", "Content-Type"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
+
+	server.Group("/api/users", func (r martini.Router) {
+		r.Get("/", getUsers)
+		r.Post("/", insertUser)
+		r.Get("/:id", getUser)
+		r.Put("/:id", updateUser)
+		r.Delete("/:id", deleteUser)
+	})
+
+	log.Info("Server listening on port 3000")
+
+	server.Run()
+}
